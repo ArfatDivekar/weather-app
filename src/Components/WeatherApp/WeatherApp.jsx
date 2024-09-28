06a4f95caa4d54aef28eaebaec313d0459b8677e
ArfatDivekar2109 <fonetour99@gmail.com>
2024-09-28 12:59:27 +0530

    third commit

--- src/Components/WeatherApp/WeatherApp.jsx
+++ src/Components/WeatherApp/WeatherApp.jsx
@@ -2,19 +2,19 @@ import React, { useState } from "react";
 import axios from "axios";
 
 function WeatherApp() {
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
 
-  const [data, setData] = useState({})
-  const [location, setLocation] = useState('')
-  const [errorMessage, setErrorMessage] = useState('');
-
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=a4ca3a81bdb12e3535055e1442e7cdc9`
+  const apiKey = process.env.REACT_APP_WEATHER_API_KEY;
 
   const searchLocation = async (event) => {
-    if (event.key === 'Enter') {
+    if (event.key === "Enter") {
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=${apiKey}`;
       try {
         const response = await axios.get(url);
         setData(response.data);
-        setErrorMessage('');
+        setErrorMessage("");
         console.log(response.data);
       } catch (error) {
         if (error.response && error.response.status === 404) {
@@ -22,25 +22,27 @@ function WeatherApp() {
           setData({});
         } else {
           console.error("An error occurred while fetching data:", error);
-          setErrorMessage("An error occurred while fetching data. Please try again.");
+          setErrorMessage(
+            "An error occurred while fetching data. Please try again."
+          );
         }
       }
-      setLocation('');
+      setLocation("");
     }
   };
 
   return (
     <div className="app">
       <div className="search">
-        <input 
-          value= {location} 
-          onChange={event => setLocation(event.target.value)} 
+        <input
+          value={location}
+          onChange={(event) => setLocation(event.target.value)}
           onKeyPress={searchLocation}
           placeholder="Search..."
-          type="text" 
+          type="text"
         />
         <div className="title">
-            <h2>Weather App 1.0</h2>
+          <h2>Weather App 1.0</h2>
         </div>
       </div>
       <div className="container">
@@ -50,36 +52,44 @@ function WeatherApp() {
           </div>
 
           <div className="temp">
-              {data.main ? ( <h1>{Math.round((data.main.temp - 32) * 5 / 9)}<sup>o</sup>C</h1> ) : null}
+            {data.main ? (
+              <h1>
+                {Math.round(((data.main.temp - 32) * 5) / 9)}
+                <sup>o</sup>C
+              </h1>
+            ) : null}
 
-              {errorMessage && <h2>{errorMessage}</h2>}
+            {errorMessage && <h2>{errorMessage}</h2>}
           </div>
 
-
           <div className="weather-desc">
-          {data.weather ? <p>{data.weather[0].main}</p> : null}
+            {data.weather ? <p>{data.weather[0].main}</p> : null}
           </div>
         </div>
 
-        {data.name != undefined && 
+        {data.name != undefined && (
           <div className="bottom">
             <div className="feels">
-              {data.main ? (<p className="bold">{Math.round((data.main.feels_like - 32) * 5 / 9)}<sup>o</sup>C</p>) : null}
+              {data.main ? (
+                <p className="bold">
+                  {Math.round(((data.main.feels_like - 32) * 5) / 9)}
+                  <sup>o</sup>C
+                </p>
+              ) : null}
               <p className="default-text">Feels like</p>
             </div>
 
             <div className="humidity">
-              {data.main ? (<p className="bold">{data.main.humidity}%</p>) : null}
-              <p className="default-text">Humdity</p>
+              {data.main ? <p className="bold">{data.main.humidity}%</p> : null}
+              <p className="default-text">Humidity</p>
             </div>
 
             <div className="wind">
-            {data.main ? (<p className="bold">{data.wind.speed}</p>) : null}
+              {data.main ? <p className="bold">{data.wind.speed}</p> : null}
               <p className="default-text">Wind speed</p>
             </div>
           </div>
-        } 
-
+        )}
       </div>
     </div>
   );
